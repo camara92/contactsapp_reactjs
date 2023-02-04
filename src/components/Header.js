5bb2a637575f425b0841ae776e447f0e648f1885
CAMARA <88945535+camara92@users.noreply.github.com>
2023-02-04 01:33:02 +0100

    components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className='ui fixed menu '>
+                <div className='ui container center'>
+                    <h2>Contact manager </h2>
+                </div>
+            </div>
+        )
+    }
+}
